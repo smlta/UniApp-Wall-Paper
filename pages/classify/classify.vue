be9a42bc4c40e465367d81cafff1ae0dd36e4f09
Alice <2162823020@qq.com>
2025-11-15 23:28:46 +0800

    分类页布局初始化

---
+++ pages/classify/classify.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+	<view class="classLayout">
+		<view class="classify">
+			<theme-item v-for="item in 15"></theme-item>
+		</view>
+	</view>
+</template>
+
+<style lang="scss" scoped>
+.classLayout {
+	.classify {
+		padding: 30rpx;
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 15rpx;
+	}
+}
+</style>
